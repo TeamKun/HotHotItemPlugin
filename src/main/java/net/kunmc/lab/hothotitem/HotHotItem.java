85d2bfb01d37ea7855b29b5860d15e64344cc00e
McNagatuki <kikihiji.fuumi5447491@gmail.com>
2021-02-14 22:48:25 +0900

    feat: damage increasing based on the number of filled slots

--- src/main/java/net/kunmc/lab/hothotitem/HotHotItem.java
+++ src/main/java/net/kunmc/lab/hothotitem/HotHotItem.java
@@ -1,8 +1,15 @@
 package net.kunmc.lab.hothotitem;
 
+import org.bukkit.GameMode;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
+
+import java.util.Arrays;
 
 public final class HotHotItem extends JavaPlugin implements Listener {
     public static HotHotItem plugin;
@@ -42,5 +49,24 @@ public final class HotHotItem extends JavaPlugin implements Listener {
 
     public boolean isRunning() {
         return running;
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event) {
+        if (!isRunning()) return;
+
+        EntityDamageEvent.DamageCause cause = event.getCause();
+        if (cause != EntityDamageEvent.DamageCause.FIRE_TICK) return;
+
+        Entity entity = event.getEntity();
+        if (!(entity instanceof Player)) return;
+
+        Player player = (Player) entity;
+        if (player.getGameMode() == GameMode.CREATIVE) return;
+        if (player.getGameMode() == GameMode.SPECTATOR) return;
+
+        long count = Arrays.stream(player.getInventory().getContents()).filter(v -> v != null).count();
+        double damage = config.getCoefficient() * (count - 1) + config.getDamage();
+        event.setDamage(damage);
     }
 }
