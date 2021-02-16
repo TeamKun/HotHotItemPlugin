9d7f5a4e9b65898f84491080083d962d3e7c7818
Nagatuki <63790343+McNagatuki@users.noreply.github.com>
2021-02-17 00:50:55 +0900

    Damage in the water (#1)
    * feat: damage in the water
    
    * refactor: wrap the possession time of items

--- src/main/java/net/kunmc/lab/hothotitem/Task.java
+++ src/main/java/net/kunmc/lab/hothotitem/Task.java
@@ -1,8 +1,8 @@
 package net.kunmc.lab.hothotitem;
 
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Arrays;
@@ -10,40 +10,77 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Task extends BukkitRunnable {
-    // item holding time (ticks)
-    private Map<Player, Integer> count = new HashMap<>();
+    // item holding times (ticks)
+    private Map<Player, Integer> holdingTimes = new HashMap<>();
 
-    private boolean hasItem(Player player) {
-        PlayerInventory inventory = player.getInventory();
-        return Arrays.stream(inventory.getContents()).anyMatch(v -> v != null);
+    private HotHotItem plugin;
+    private Config config;
+
+    Task() {
+        plugin = HotHotItem.plugin;
+        config = plugin.config;
+    }
+
+    private long countFilledSlots(Player player) {
+        return Arrays.stream(player.getInventory().getContents()).filter(v -> v != null).count();
+    }
+
+    private boolean validatePlayer(Player player) {
+        if (!player.isOnline()) return false;
+        if (player.getGameMode() == GameMode.CREATIVE) return false;
+        if (player.getGameMode() == GameMode.SPECTATOR) return false;
+        return true;
+    }
+
+    private boolean isInWater(Player player) {
+        Material material = player.getLocation().getBlock().getType();
+        return material == Material.WATER;
+    }
+
+    private void hurtPlayer(Player player, long count) {
+        if (count == 0) return;
+        double damage = config.getCoefficient() * (count - 1) + config.getDamage();
+        player.damage(damage);
+    }
+
+    private void putHoldingTime(Player player, int value) {
+        holdingTimes.put(player, value);
+    }
+
+    private int getHoldingTime(Player player) {
+        if (!holdingTimes.containsKey(player)) return 0;
+        return holdingTimes.get(player);
     }
 
     @Override
     public void run() {
-        HotHotItem plugin = HotHotItem.plugin;
-        int period = plugin.config.getPeriod();
-        double time = plugin.config.getTime();
+        int period = config.getPeriod();
+        double time = config.getTime();
 
         for (Player player : plugin.getServer().getOnlinePlayers()) {
-            // player validations
-            if (!player.isOnline()) continue;
-            if (player.getGameMode() == GameMode.CREATIVE) continue;
-            if (player.getGameMode() == GameMode.SPECTATOR) continue;
+            if (!validatePlayer(player)) continue;
 
-            if (!count.containsKey(player)) count.put(player, 0);
+            long count = countFilledSlots(player);
 
-            // get holding time (ticks)
-            int holdingTime = count.get(player);
-
-            holdingTime = hasItem(player) ? holdingTime + period : 0;
-
-            // if player has item(s) too long...
-            if (holdingTime > time * 20) {
-                player.setFireTicks(period + 1);
-                holdingTime = (int) (time * 20) + 1; // prevent overflow
+            if (count == 0) {
+                putHoldingTime(player, 0);
+                continue;
             }
 
-            count.put(player, holdingTime);
+            // get holding time (ticks) and update
+            int holdingTime = getHoldingTime(player) + period;
+            holdingTime = Math.min(holdingTime, (int) (time * 20) + 1); // prevent overflow
+            putHoldingTime(player, holdingTime);
+
+            // if player is in the water, damage is decreased.
+            if (isInWater(player)) --count;
+
+            // if the player has item(s) too long...
+            if (holdingTime > time * 20) {
+                player.setFireTicks(period + 1);
+                hurtPlayer(player, count);
+            }
+
         }
     }
 }
