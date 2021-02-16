ca9786dc6cedb0d3ab45ce74763a35b04610f54a
McNagatuki <kikihiji.fuumi5447491@gmail.com>
2021-02-16 20:17:12 +0900

    feat: damage in the water

--- src/main/java/net/kunmc/lab/hothotitem/HotHotItem.java
+++ src/main/java/net/kunmc/lab/hothotitem/HotHotItem.java
@@ -1,17 +1,9 @@
 package net.kunmc.lab.hothotitem;
 
-import org.bukkit.GameMode;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
-import java.util.Arrays;
-
-public final class HotHotItem extends JavaPlugin implements Listener {
+public final class HotHotItem extends JavaPlugin {
     public static HotHotItem plugin;
     public Config config;
     private BukkitTask task;
@@ -22,7 +14,6 @@ public final class HotHotItem extends JavaPlugin implements Listener {
         plugin = this;
         config = new Config();
 
-        getServer().getPluginManager().registerEvents(this, this);
         this.getCommand("hotitem").setExecutor(new CommandManager());
     }
 
@@ -49,24 +40,5 @@ public final class HotHotItem extends JavaPlugin implements Listener {
 
     public boolean isRunning() {
         return running;
-    }
-
-    @EventHandler
-    public void onDamage(EntityDamageEvent event) {
-        if (!isRunning()) return;
-
-        EntityDamageEvent.DamageCause cause = event.getCause();
-        if (cause != EntityDamageEvent.DamageCause.FIRE_TICK) return;
-
-        Entity entity = event.getEntity();
-        if (!(entity instanceof Player)) return;
-
-        Player player = (Player) entity;
-        if (player.getGameMode() == GameMode.CREATIVE) return;
-        if (player.getGameMode() == GameMode.SPECTATOR) return;
-
-        long count = Arrays.stream(player.getInventory().getContents()).filter(v -> v != null).count();
-        double damage = config.getCoefficient() * (count - 1) + config.getDamage();
-        event.setDamage(damage);
     }
 }
--- src/main/java/net/kunmc/lab/hothotitem/Task.java
+++ src/main/java/net/kunmc/lab/hothotitem/Task.java
@@ -1,8 +1,8 @@
 package net.kunmc.lab.hothotitem;
 
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Arrays;
@@ -10,40 +10,70 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Task extends BukkitRunnable {
-    // item holding time (ticks)
-    private Map<Player, Integer> count = new HashMap<>();
+    // item holding times (ticks)
+    private Map<Player, Integer> holdingTimes = new HashMap<>();
 
-    private boolean hasItem(Player player) {
-        PlayerInventory inventory = player.getInventory();
-        return Arrays.stream(inventory.getContents()).anyMatch(v -> v != null);
+    private HotHotItem plugin;
+    private Config config;
+
+    Task() {
+        plugin = HotHotItem.plugin;
+        config = plugin.config;
+    }
+
+    private long countFilledSlots(Player player) {
+        return Arrays.stream(player.getInventory().getContents()).filter(v -> v != null).count();
+    }
+
+    private boolean validatePlayer(Player player) {
+        if (!player.isOnline()) return false;
+        if (player.getGameMode() == GameMode.CREATIVE) return false;
+        if (player.getGameMode() == GameMode.SPECTATOR) return false;
+        return true;
+    }
+
+    private boolean isInWater(Player player) {
+        Material material = player.getLocation().getBlock().getType();
+        return material == Material.WATER;
+    }
+
+    private void hurtPlayer(Player player, long count) {
+        if (count == 0) return;
+        double damage = config.getCoefficient() * (count - 1) + config.getDamage();
+        player.damage(damage);
     }
 
     @Override
     public void run() {
-        HotHotItem plugin = HotHotItem.plugin;
-        int period = plugin.config.getPeriod();
-        double time = plugin.config.getTime();
+        int period = config.getPeriod();
+        double time = config.getTime();
 
         for (Player player : plugin.getServer().getOnlinePlayers()) {
-            // player validations
-            if (!player.isOnline()) continue;
-            if (player.getGameMode() == GameMode.CREATIVE) continue;
-            if (player.getGameMode() == GameMode.SPECTATOR) continue;
+            if (!validatePlayer(player)) continue;
 
-            if (!count.containsKey(player)) count.put(player, 0);
+            if (!holdingTimes.containsKey(player)) holdingTimes.put(player, 0);
 
-            // get holding time (ticks)
-            int holdingTime = count.get(player);
+            long count = countFilledSlots(player);
 
-            holdingTime = hasItem(player) ? holdingTime + period : 0;
-
-            // if player has item(s) too long...
-            if (holdingTime > time * 20) {
-                player.setFireTicks(period + 1);
-                holdingTime = (int) (time * 20) + 1; // prevent overflow
+            if (count == 0) {
+                holdingTimes.put(player, 0);
+                continue;
             }
 
-            count.put(player, holdingTime);
+            // get holding time (ticks) and update
+            int holdingTime = holdingTimes.get(player) + period;
+            holdingTime = Math.min(holdingTime, (int) (time * 20) + 1); // prevent overflow
+            holdingTimes.put(player, holdingTime);
+
+            // if player is in the water, damage is decreased.
+            if (isInWater(player)) --count;
+
+            // if the player has item(s) too long...
+            if (holdingTime > time * 20) {
+                player.setFireTicks(period + 1);
+                hurtPlayer(player, count);
+            }
+
         }
     }
 }
